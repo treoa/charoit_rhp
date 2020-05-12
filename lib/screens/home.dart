5cc194917a22469be926fd4daf291e6d5ccf931b
treoa <anuar.suleimenov@nu.edu.kz>
2020-05-13 04:57:30 +0600

    Initial Firebase configs

---
+++ lib/screens/home.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  
+  final FirebaseUser user;
+  
+  const HomePage({Key key, this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(32),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("TREECAPITATOR, You are logged in, dear ",
+            style: TextStyle(),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,82 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:charoit_rhp/Animation/FadeAnimation.dart';
 
 class LoginPage extends StatelessWidget {
-  final _formkey = new GlobalKey<FormState>();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
-        color: Color.fromRGBO(36, 43, 47, 1),
-        padding: const EdgeInsets.symmetric(horizontal: 43.0),
-        child: Form(
-          key: _formkey,
-          child: Container(
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Stack(
+              alignment: Alignment.center,
               children: <Widget>[
-                _buildPhoneNumber(),
+                Image.asset(
+                  "assets/images/city1.png",
+                  scale: 10,
+                ),
+                Positioned(
+                  top: 160,
+                  child: Fade(
+                    2.4,
+                    Image.asset(
+                      "assets/images/sport_blue_car.png",
+                      scale: 5.6,
+                    ),
+                  ),
+                )
               ],
-            )
-          )
-        )
-      )
-    );
-  }
-
-  InputDecoration _buildInputDecoration(String hint, String iconPath) {
-    return InputDecoration(
-      focusedBorder: UnderlineInputBorder (
-        borderSide: BorderSide(color: Color.fromRGBO(252, 252, 252, 1)),
-      ),
-      hintText: hint,
-      enabledBorder: UnderlineInputBorder(
-        borderSide:BorderSide (color: Color.fromRGBO(151, 151, 151, 1)),
-      ),
-      hintStyle: TextStyle(
-        color: Color.fromRGBO(252, 252, 252, 1),
-      ),
-      icon: iconPath != '' ? Image.asset(iconPath) : null,
-      errorStyle: TextStyle(
-        color: Color.fromRGBO(248, 218, 87, 1),
-      ),
-      errorBorder: UnderlineInputBorder(
-        borderRadius: BorderRadius.circular(24),
-        borderSide: BorderSide(
-          color: Color.fromRGBO(248, 218, 87, 1),
-        ),
-      ),
-      focusedErrorBorder: UnderlineInputBorder(
-        borderSide: BorderSide(
-          color: Color.fromRGBO(248, 218, 87, 1),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+          ],
         ),
       ),
     );
-  }
-
-  Widget _buildPhoneNumber () {
-    final _PhoneNumberController = TextEditingController();
-
-    return TextFormField(
-      obscureText: false,
-      controller: _PhoneNumberController,
-      validator: (value) => !isPhoneNumber(value) ? "Неправильный номер телефона" : null,
-      style: TextStyle(
-        color: Color.fromRGBO(252, 252, 252, 1),
-      ),
-      // decoration: _buildInputDecoration("Phone Number", "assets/images/background.png"),
-    );
-  }
-
-  bool isPhoneNumber(String value) {
-    String regex = r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$';
-
-    RegExp regExp = new RegExp(regex);
-
-    return value.isNotEmpty && regExp.hasMatch(value);
   }
 }
